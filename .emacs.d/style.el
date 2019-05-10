0f94f669212f493a441bc664679683c31ed43cb7
Thomas Churchman <thomas@kepow.org>
2019-05-10 13:56:30 +0200

    Replace modeline with Moody.

--- .emacs.d/style.el
+++ .emacs.d/style.el
@@ -1,7 +1,13 @@
-(use-package smart-mode-line
-  :config             
-  (setq sml/theme 'dark)
-  (sml/setup))
+(use-package moody
+  :config
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (setq line-number-mode t)
+  (setq column-number-mode t))
+(use-package minions
+  :config (minions-mode 1))
+
 (use-package zenburn-theme)
 ;; (use-package fill-column-indicator
 ;;  :config
